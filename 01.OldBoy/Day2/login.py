22fd11f850a57a819265e69cf8a7e45fd7e90d49
joraboy328 <gang.liu@dic.com.cn>
2017-07-25 21:30:02 +0800

    Day2

---
+++ 01.OldBoy/Day2/login.py
@@ -0,0 +1,10 @@
+_username = 'liugang'
+_password = '1234'
+
+user = input ("please input username:")
+pwd = input ("please input password")
+
+if user == _username and pwd == _password:
+    print("welcome user %s login" %user)
+else:
+    print("Wrong username or password")
---
+++ 01.OldBoy/Day2/mysys.py
@@ -0,0 +1,7 @@
+import sys
+
+print(sys.path)
+print(sys.platform)
+print(sys.version)
+print(sys.argv)
+'''print(sys.argv[1])'''
