27ac07b7e159ebc9f3c7d5247e41632415e6a087
joraboy328 <gang.liu@dic.com.cn>
2017-08-02 07:28:45 +0800

    2017/8/1 1

---
+++ 01.OldBoy/Day2/3level_menu.py
@@ -0,0 +1,47 @@
+data = {
+
+    'beijin':{
+        'changping':{
+            "guomao":["xx","bb"],
+            "tiandu":{"xxx","yyy"}
+        }
+
+    },
+    'jiangsu': {
+        'nantong': {
+            "chongchuan": ["wanda", "jinbaobei"],
+            "gangzha": {"huoguo", "ruchuan"}
+        },
+        'suzhou':{
+            "def":["xxxxxxx","y"],
+            "abc":["xxxx","yyyyy"]
+        }
+
+
+    }
+}
+
+while True:
+ for i in data:
+    print(i)
+ choice = input("please input=>")
+ if choice in data:
+    while True:
+        for i2 in data[choice]:
+            print("\t",i2)
+        choice2 = input("please input=> or Press[B] return")
+        if choice2 == "B" or "b":
+            break
+        if choice2 in data[choice]:
+            while True:
+                for i3 in data[choice][choice2]:
+                    print("\t",i3)
+                choice3 = input("please input=>")
+                for i4 in data[choice][choice2][choice3]:
+                    print(i4)
+                choice4 = input("Press [B] return")
+                if choice4 == "B":
+                    break
+
+
+
